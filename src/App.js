bebf6148953e2504b4aedc527cd6ac3331b138ca
adria <adrian70000000@gmail.com>
2025-10-21 12:37:44 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function App()
+{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={"root"}>
+        <header className={"a"}>
+            ma powiedzieć gdzie jesteśmy (konkretne koordymaty).
+        </header>
+
+        <section className={"b"}>
+            b
+        </section>
+
+        <section className={"c"}>
+            c
+        </section>
+
+        <aside className={"d"}>
+            d
+        </aside>
+
+        <article className={"e"}>
+            e
+        </article>
+
+        <footer className={"f"}>
+            f
+        </footer>
     </div>
   );
 }
