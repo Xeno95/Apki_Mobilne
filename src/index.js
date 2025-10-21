dfd8fad49b66136512b8615ee16cb727e22d7ec2
adria <adrian70000000@gmail.com>
2025-10-21 13:07:09 +0200

    .

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './other/reportWebVitals';
-import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HashRouter><App/></HashRouter>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
